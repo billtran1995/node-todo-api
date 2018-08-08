8338bea07853765a6964fc915be6a4b39bf01009
Bill Tran <redbill1995@gmail.com>
2018-08-07 21:01:39 -0500

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 /* ----------------------------------------------------------------------------------*/
 // User model
@@ -81,10 +82,25 @@ UserSchema.statics.findByToken = function(token) {
 
   return User.findOne({
     _id: decoded._id,
-    "tokens.token": token,
+    "tokens.token": token, // Use " " to query nested items
     "tokens.access": "auth"
   });
 };
+
+UserSchema.pre("save", function(next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hashedpassword) => {
+        user.password = hashedpassword;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model("User", UserSchema);
 
